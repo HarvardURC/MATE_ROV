ff9f626c93d620209e9df1c8b7d1910aecdfba1e
Gunnar Plunkett <glplunkett@college.harvard.edu>
2017-10-26 23:01:10 -0400

    host and port variables updated

--- server.py
+++ server.py
@@ -8,6 +8,13 @@ from flask import Flask, send_file, render_template, request, url_for, redirect
 ADDRESS = os.environ.get("BIND_ADDRESS","localhost")
 PORT = os.environ.get("BIND_PORT", 11297)
 
+# flask stuff
+host = ADDRESS
+port = PORT
+
+app = Flask(__name__)
+
+@app.route("/")
 def main():
     server = robomodules.Server(ADDRESS, PORT, MsgType)
     server.run()
