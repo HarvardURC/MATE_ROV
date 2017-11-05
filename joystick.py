9ced48e4ed3f178ea3740f1270d7f1040c7e7775
Gunnar Plunkett <glplunkett@college.harvard.edu>
2017-11-05 11:07:07 -0500

    made sure changes work win GUIModule class

--- joystick.py
+++ joystick.py
@@ -52,42 +52,43 @@ class GUIModule(rm.ProtoModule):
 		# for this mock module we will just send a random int
 		msg = CtrlMsg()
 		input()
-		msg.x = x
-		msg.y = y
-		msg.z = z
-		msg.pitch = pitch
-		msg.yaw = yaw
-		msg.roll = roll
+		msg.x = self.x
+		msg.y = self.y
+		msg.z = self.z
+		msg.pitch = self.pitch
+		msg.yaw = self.yaw
+		msg.roll = self.roll
 		msg = msg.SerializeToString()
 		self.write(msg, MsgType.CTRL_MSG)
 
 	def _get_input(self):
-		buttonA = logitech.get_button(1)
-		buttonB = logitech.get_button(2)
-		buttonX = logitech.get_button(0)
-		buttonY = logitech.get_button(3)
-		buttonLB = logitech.get_button(4)
-		buttonRB = logitech.get_button(5)
-		buttonLT = logitech.get_button(6)
-		buttonRT = logitech.get_button(7)
+		joy = self.logitech
+		buttonA = joy.get_button(1)
+		buttonB = joy.get_button(2)
+		buttonX = joy.get_button(0)
+		buttonY = joy.get_button(3)
+		buttonLB = joy.get_button(4)
+		buttonRB = joy.get_button(5)
+		buttonLT = joy.get_button(6)
+		buttonRT = joy.get_button(7)
 
 		# -1 <= up < 0 < down <= +1
 		if buttonLB:
 			y = 0.
-			z = logitech.get_axis(1)
+			z = joy.get_axis(1)
 		else:
-			y = logitech.get_axis(1)
+			y = joy.get_axis(1)
 			z = 0.
-		pitch = logitech.get_axis(3)
+		pitch = joy.get_axis(3)
 
 		# -1 <= left < 0 < right <= +1
 		if buttonRB:
 			yaw = 0.
-			roll = logitech.get_axis(2)
+			roll = joy.get_axis(2)
 		else:
-			yaw = logitech.get_axis(2)
+			yaw = joy.get_axis(2)
 			roll = 0.
-		x = logitech.get_axis(0)
+		x = joy.get_axis(0)
 
 
 def main():
