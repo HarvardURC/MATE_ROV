0607a3132ea052ae45709b72f9e2bb7cd35ac345
Gunnar Plunkett <glplunkett@college.harvard.edu>
2017-11-05 17:39:51 -0500

    added imu mock skeleton code

---
+++ imuModule.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os, random
+import robomodules as rm
+from messages import *
+
+ADDRESS = os.environ.get("BIND_ADDRESS","localhost")
+PORT = os.environ.get("BIND_PORT", 11297)
+
+FREQUENCY = 2
+
+class MockSensorModule(rm.ProtoModule):
+    def __init__(self, addr, port):
+        self.subscriptions = []
+        super().__init__(addr, port, message_buffers, MsgType, FREQUENCY)
+
+    def msg_received(self, msg, msg_type):
+        # This gets called whenever any message is received
+        # This module only sends data, so we ignore incoming messages
+        return
+
+    def tick(self):
+        # this function will get called in a loop with FREQUENCY frequency
+        # for this mock module we will just send a random int
+        msg = MockMsg()
+        msg.mockValue = random.randint(1, 9)
+        msg = msg.SerializeToString()
+        self.write(msg, MsgType.MOCK_MSG)
+
+
+def main():
+    module = MockSensorModule(ADDRESS, PORT)
+    module.run()
+
+if __name__ == "__main__":
+    main()
