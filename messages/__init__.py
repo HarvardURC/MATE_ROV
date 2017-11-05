c38cbef59815075e1d8ba3d61a472e392eb4d2ef
Gunnar Plunkett <glplunkett@college.harvard.edu>
2017-11-05 10:54:51 -0500

    requested changes

--- messages/__init__.py
+++ messages/__init__.py
@@ -1,12 +1,15 @@
 from enum import Enum
 from .mockMsg_pb2 import MockMsg
+from .ctrlMsg_pb2 import CtrlMsg
 
 class MsgType(Enum):
     MOCK_MSG = 0
+    CTRL_MSG = 1
 
 message_buffers = {
     MsgType.MOCK_MSG: MockMsg
+    MsgType.CTRL_MSG: CtrlMsg
 }
 
 
-__all__ = ['MsgType', 'message_buffers', 'MockMsg']
+__all__ = ['MsgType', 'message_buffers', 'MockMsg', 'CtrlMsg']
