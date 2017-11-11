8d985c22438abc17a4750422ccb765563422f56d
Gunnar Plunkett <glplunkett@college.harvard.edu>
2017-11-11 15:58:05 -0500

    controls servo acceleration, probably

--- servoModule.py
+++ servoModule.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import maestro.py
 import time
 import wiringpi
 import os, random
@@ -9,33 +10,37 @@ from messages import *
 ADDRESS = os.environ.get("BIND_ADDRESS","localhost")
 PORT = os.environ.get("BIND_PORT", 11297)
 
-FREQUENCY = 2
+FREQUENCY = 20
 
-class MockSensorModule(rm.ProtoModule):
+class ServoModule(rm.ProtoModule):
     def __init__(self, addr, port):
         self.subscriptions = []
 
         # use 'GPIO naming'
         wiringpi.wiringPiSetupGpio()
 
+        self.servo = maestro.Controller()
+
         super().__init__(addr, port, message_buffers, MsgType, FREQUENCY)
 
     def msg_received(self, msg, msg_type):
         # This gets called whenever any message is received
         # This module only sends data, so we ignore incoming messages
+        if msg_type == MsgType.CTRL_MSG:
+            self._servo_ctrl(msg.servo0, msg.servo1)
         return
 
     def tick(self):
-        # this function will get called in a loop with FREQUENCY frequency
-        # for this mock module we will just send a random int
-        msg = MockMsg()
-        msg.mockValue = random.randint(1, 9)
-        msg = msg.SerializeToString()
-        self.write(msg, MsgType.MOCK_MSG)
+        self.subscribe(MsgType.CTRL_MSG)
+
+    def _servo_ctrl(servoX, servoY):
+        servo.setAccel(0, 4*servoX)
+        servo.setAccel(1, 4*servoY)
+        
 
 
 def main():
-    module = MockSensorModule(ADDRESS, PORT)
+    module = ServoModule(ADDRESS, PORT)
     module.run()
 
 if __name__ == "__main__":
