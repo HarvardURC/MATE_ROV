250767044d7fbf3f20bbb9ad54cc227e8577cb61
timothy Tamm <tamm@college.harvard.edu>
2017-11-11 19:07:25 -0500

    display humidity in gui

--- guiModule.py
+++ guiModule.py
@@ -24,6 +24,9 @@ class GuiModule(rm.ProtoModule):
         super().__init__(addr, port, message_buffers, MsgType, FREQUENCY, self.subscriptions)
         self.frames = {}
         pygame.init()
+        pygame.font.init()
+        self.text_surf = pygame.Surface((300, 50))
+        self.font = pygame.font.SysFont(pygame.font.get_default_font(), 30)
         self.display = pygame.display.set_mode(SCREEN_SIZE)
         self.navball = NavBall(self.display, NAVBALL_SIZE, SCREEN_SIZE[0]/2, SCREEN_SIZE[1] - NAVBALL_SIZE/2 - 50)
         self.navball_ticks = 0
@@ -41,7 +44,7 @@ class GuiModule(rm.ProtoModule):
             self.roll = msg.roll * 90
             self.pitch = msg.pitch * 90
             self.yaw = msg.yaw * 90
-        elif msg_type = MsgType.HUMIDITY_MSG
+        elif msg_type == MsgType.HUMIDITY_MSG:
             self.humidity = msg.humidity
         
     def tick(self):
@@ -56,8 +59,10 @@ class GuiModule(rm.ProtoModule):
         self.navball_ticks += 1
 
     def _draw_sensor_values(self):
-        hum_surface = self.font.render('Humidity={1:0.1f}%'.format(self.humidity), True, (255, 255, 255))
-        self.display.blit(hum_surface, (SCREEN_SIZE[0]-100, SCREEN_SIZE[1]-50))
+        self.text_surf.fill((0,0,0))
+        hum_surface = self.font.render('Humidity={0:0.1f}%'.format(self.humidity), True, (255, 255, 255))
+        self.text_surf.blit(hum_surface, (0, 0))
+        self.display.blit(self.text_surf, (SCREEN_SIZE[0]-300, SCREEN_SIZE[1]-50))
 
     def _display_frames(self):
         cur_x = 0
