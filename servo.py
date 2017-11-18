a7ad82ecaa34a91dfebd5ad2f9f9f028a1a167ea
Timothy Tamm <tamm@college.harvard.edu>
2017-11-18 07:35:40 +0000

    maestro y u no work

--- servo.py
+++
@@ -1,5 +0,0 @@
-import maestro
-servo = maestro.Controller('ttyACM1')
-#servo.setAccel(0,4)      #set servo 0 acceleration to 4
-servo.setTarget(0,6000)  #set servo to move to center position
-servo.close
